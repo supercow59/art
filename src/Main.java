e617d57227bba9304ee040b114fe862a1deaf2ef
100142500 <Spike5707>
2022-09-01 09:46:16 -0500

    changed aiplanes

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        System.out.println("    __!__\n" +
+                "_____(_)_____\n" +
+                "   !  !  !");
+    }
+}
